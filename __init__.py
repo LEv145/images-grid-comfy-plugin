75db834e18fd3b151e9ac152a5f654c0afab08b2
LEv145 <you@example.com>
2023-04-05 00:20:01 +0200

    v 1.0.3

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .src import ImageSetAreaNode, FloatImageCombineNode, XYPlotNode
-
-
-NODE_CLASS_MAPPINGS = {
-    "ImageSetArea": ImageSetAreaNode,
-    "FloatImageCombine": FloatImageCombineNode,
-    "XYPlot": XYPlotNode,
-}
--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .nodes.image_set_area import ImageSetAreaNode
-from .nodes.float_image_combine import FloatImageCombineNode
-from .nodes.xy_plot import XYPlotNode
