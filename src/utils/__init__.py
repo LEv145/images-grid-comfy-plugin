1a05160eb23548cd50cfca5f8ebb04fee6bdde2f
LEv145 <you@example.com>
2023-04-07 01:12:17 +0200

    v 2.1.3
    Add utils module to `__init__`

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,4 +2,5 @@ from .images_grid import (
     create_images_grid_by_columns,
     create_images_grid_by_rows,
 )
+from grid_annotations import create_grid_annotations
 from .tensor_convert import tensor_to_pillow, pillow_to_tensor
